a36f021c93ab7f09e584db0b7da3b17ae57111b6
Molly Williams <mollywilliams@Mollys-MacBook-Pro-3.local>
2021-02-01 21:41:45 -0500

    login screen

---
+++ app/src/main/java/com/example/chooseyourcocktail/SearchActivity.java
@@ -0,0 +1,21 @@
+package com.example.chooseyourcocktail;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
